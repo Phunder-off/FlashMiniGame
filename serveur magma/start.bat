71ac27bade66c2fdc3ccb0dd4335c16018685c62
Emmanuel <caronnunu@gmail.com>
2023-02-19 14:47:30 +0100

    add java

--- serveur magma/start.bat
+++ serveur magma/start.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Java\jre1.8.0_351\bin\java" -jar -Xms500m -Xmx8000m Magma-1.12.2-a0c0ffb0.jar
+"../jre1.8.0_281/bin/java" -jar -Xms500m -Xmx1000m Magma-1.12.2-a0c0ffb0.jar
 start.bat
